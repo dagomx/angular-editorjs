a2b7c5e716261d73bce0e0c31996d08d342d19f8
dagomx <dagoxl@gmail.com>
2020-03-01 00:05:57 -0600

    Includes blocks: header, list and raw (HTML)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header';
 import List from '@editorjs/list';
+import RawTool from '@editorjs/raw';
 
 @Component({
   selector: 'my-app',
@@ -15,15 +16,18 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.editor = new EditorJS({
-      /** 
-       * Id of Element that should contain the Editor 
-       */
+      // Id of Element that should contain the Editor 
       holderId: 'codex-editor',
 
-      /** 
-       * Available Tools list. 
-       * Pass Tool's class or Settings object for each Tool you want to use 
-       */
+      autofocus: true,
+      placeholder: 'Type your content here...',
+
+      onReady: () => {
+        console.log('Editor.js is ready to work!')
+      },
+
+      // Available Tools list. 
+      // Pass Tool's class or Settings object for each Tool you want to use 
       tools: {
         header: {
           class: Header,
@@ -32,16 +36,18 @@ export class AppComponent implements OnInit {
         list: {
           class: List,
           inlineToolbar: true
-        }
+        },
+        raw: RawTool
       },
     });
   }
 
   save() {
     this.editor.save().then((outputData) => {
-      console.log('Article data: ', outputData)
+      console.log('Article data: ', outputData);
+      console.log('JSON data:', JSON.stringify(outputData));
     }).catch((error) => {
-      console.log('Saving failed: ', error)
+      console.log('Saving failed: ', error);
     });
   }
 
